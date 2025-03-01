189251a09d81ee3211e7a2a92988c9c616cef12f
Muhammad Fathurrahman <muhammadfathurrahman013@gmail.com>
2025-03-02 00:24:30 +0700

    add function edit, destroy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -6,25 +7,54 @@ use App\Models\Post;
 
 class PostController extends Controller
 {
-    // Menampilkan form input
-    public function create()
+  public function index()
+  {
+    $posts = Post::all();
+    return view('posts.index', compact('posts'));
+  }
+  // Menampilkan form input
+  public function create()
+  {
+    return view('posts.create');
+  }
+
+  // Menyimpan data ke database
+  public function store(Request $request)
+  {
+    // Validasi input
+    $request->validate([
+      'title' => 'required|max:255',
+      'content' => 'required',
+    ]);
+
+    // Simpan data ke database
+    Post::create($request->all());
+
+    // Redirect ke halaman lain dengan pesan sukses
+    return redirect()->route('posts.create')->with('success', 'Post created successfully!');
+  }
+  
+  public function edit(Post $post)
     {
-        return view('posts.create');
+        return view('posts.edit', compact('post'));
     }
 
-    // Menyimpan data ke database
-    public function store(Request $request)
+    public function update(Request $request, Post $post)
     {
-        // Validasi input
         $request->validate([
             'title' => 'required|max:255',
             'content' => 'required',
         ]);
 
-        // Simpan data ke database
-        Post::create($request->all());
+        $post->update($request->all());
 
-        // Redirect ke halaman lain dengan pesan sukses
-        return redirect()->route('posts.create')->with('success', 'Post created successfully!');
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully.');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully.');
     }
 }
