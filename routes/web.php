704f7dcee11be0db7e3a419006bc1b6bf2f1867a
Muhammad Fathurrahman <muhammadfathurrahman013@gmail.com>
2025-03-02 02:18:17 +0700

    clear routes

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::resource('posts', PostController::class);
-
-Route::get('/pendidikan', function () {
-    return view('berita', ['title' => 'Pendidikan']);
-});
-
-Route::get('/ekonomi', function () {
-    return view('berita', ['title' => 'Ekonomi']);
-});
-
-Route::get('/olahraga', function () {
-    return view('berita', ['title' => 'Olahraga']);
-});
-
-Route::get('/wisata', function () {
-    return view('berita', ['title' => 'Wisata']);
-});
