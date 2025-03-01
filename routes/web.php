63a55896983db1086572d14e8a673d448c81ee72
Muhammad Fathurrahman <muhammadfathurrahman013@gmail.com>
2025-03-02 02:17:07 +0700

    clear routes

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-// Menampilkan form input
-// Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
-// Menyimpan data
-// Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
-
 Route::resource('posts', PostController::class);
-
-// Route::get('/', function () {
-//     return view('index', ['posts' => Post::all()]);
-// });
 
 Route::get('/pendidikan', function () {
     return view('berita', ['title' => 'Pendidikan']);
