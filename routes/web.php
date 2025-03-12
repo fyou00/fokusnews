e538dd709bac92bcf207a5207fd4ff5651408495
Muhammad Fathurrahman <muhammadfathurrahman013@gmail.com>
2025-03-12 07:54:09 +0700

    menambah route untuk index

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::resource('posts', PostController::class);
+
+Route::get('/', function() {
+  return view('index');
+});
