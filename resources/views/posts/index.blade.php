843df11a04e2d77bee4e32fa9147e23b6b3b0661
Muhammad Fathurrahman <muhammadfathurrahman013@gmail.com>
2025-03-15 01:53:08 +0700

    fix indent

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,37 +1,35 @@
 @extends('components/layout')
 
 @section('konten')
-    <div class="container px-5">
-        <a href="posts/create" class="btn btn-primary">create posts</a>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Content</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($posts as $post)
-                    <tr>
-                        <td>{{ $post->title }}</td>
-                        <td>{{ $post->content }}</td>
-                        <td>
-                            <a href="{{ route('posts.edit', $post) }}" class="btn btn-success">Edit</a>
-                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                data-bs-target="#exampleModal">
-                                Launch demo modal
-                            </button>
-                            <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display:inline;">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger" type="submit"
-                                    onclick="return confirm('Are you sure?')">Delete</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </div>
+  <div class="container px-5">
+    <a href="posts/create" class="btn btn-primary">create posts</a>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Content</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($posts as $post)
+          <tr>
+            <td>{{ $post->title }}</td>
+            <td>{{ $post->content }}</td>
+            <td>
+              <a href="{{ route('posts.edit', $post) }}" class="btn btn-success">Edit</a>
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Launch demo modal
+              </button>
+              <form action="{{ route('posts.destroy', $post) }}" method="POST" style="display:inline;">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
 @endsection
