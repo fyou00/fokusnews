54fe8bf591a1ff63f8add80784328c111c0eea02
Muhammad Fathurrahman <muhammadfathurrahman013@gmail.com>
2025-03-15 00:42:45 +0700

    membuat link posts

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,7 +9,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Posts</a>
+            <a class="nav-link active" aria-current="page" href="/posts">Posts</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/pendidikan">Pendidikan</a>
